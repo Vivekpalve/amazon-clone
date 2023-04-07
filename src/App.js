4ea96b7519b6ed78322cc703d7e009df49c15657
vivek palve <vivekpalve9067@gmail.com>
2023-04-07 22:51:59 +0530

    router add for checkout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import Checkout from "./Components/Checkout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
