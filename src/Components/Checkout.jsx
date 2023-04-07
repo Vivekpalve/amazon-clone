223a52706b8c6ab437f4663a8abe5aa44bf311e1
vivek palve <vivekpalve9067@gmail.com>
2023-04-07 23:10:16 +0530

    adding checkout

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,13 +1,11 @@
-
-import React from 'react'
-import '../Components/Checkout.css';
+import React from "react";
+import "../Components/Checkout.css";
 const Checkout = () => {
   return (
     <div>
-        <h1>Hello</h1>
+      <h1>check</h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default Checkout;
