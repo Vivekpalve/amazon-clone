7f7c778a7646d3c3b49a4e6c3a63d6223c24ef94
vivek palve <vivekpalve9067@gmail.com>
2023-04-07 15:53:37 +0530

    i add th

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkouts">
+            <Header />
+            <h1>Hello you can checkout all the items from here n </h1>
+          </Route>
+          <Route path="/">
+            {/* Header */}
+            <Header />
+            {/* Home */}
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
